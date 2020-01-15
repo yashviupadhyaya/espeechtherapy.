74b7e264e91862d2ebf2213ba6aae5dcb7c04a08
Yashvi Upadhyaya <41316505+YASHVIU@users.noreply.github.com>
2020-01-15 18:19:45 +0530

    host

--- pages/logout.js
+++
@@ -1,7 +0,0 @@
-const btnLogout = document.getElementById('btnLogout');
-btnLogout.addEventListener('click', e => {
-    firebase.auth().signOut();
-    setTimeout(function(){
-        window.location.replace('login.html');
-    },5000);
-});
